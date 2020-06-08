69ed648d009929c6fa883f031604744c2ccd85ea
Sara Bretes <sarabretes@me.com>
2020-06-08 20:15:34 +0100

    converted hooks to pomodoro

--- src/App.js
+++ src/App.js
@@ -1,34 +1,51 @@
-import React, { useState, useEffect } from 'react'
-import './App.css'
-import { formatTime } from './utilities'
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import { formatTime } from "./utilities";
 
 const App = () => {
-  const [time, setTime] = useState(0)
-  const [timer, setTimer] = useState(false)
-  const [timerInterval, setTimerInterval] = useState(50)
-  const triggerTimer = () => setTimer(!timer)
-  const onChangeTimeInterval = (e) => setTimerInterval(+e.target.value)
+  const [timer, setTimer] = useState(false);
+  const [timerInterval, setTimerInterval] = useState(1000);
+  const [pomodoro, setPomodoro] = useState({
+    isBreak: false,
+    loopCounter: 0,
+    time: 0,
+  });
+  const triggerTimer = () => setTimer(!timer);
+  const onChangeTimeInterval = (e) => setTimerInterval(+e.target.value);
 
   useEffect(() => {
-    setTimeout(() => timer && setTime(time + timerInterval), timerInterval)
-  }, [time, timer, timerInterval])
+    console.log(pomodoro.time, timer, timerInterval, pomodoro);
+    setTimeout(() => {
+      if (timer) {
+        if (pomodoro.time < 25000) {
+          setPomodoro({ ...pomodoro, time: pomodoro.time + timerInterval });
+        } else {
+          setPomodoro({
+            isBreak: true,
+            loopCounter: pomodoro.loopCounter + 1,
+            time: 0,
+          });
+        }
+      }
+    }, timerInterval);
+  }, [pomodoro.time, timer, timerInterval, pomodoro]);
 
   return (
-    <div className='app'>
-      <div className='timer'>{formatTime(time)}</div>
-      <div>The timer is {timer ? 'on' : 'off'}</div>
+    <div className="app">
+      <div className="timer">{formatTime(pomodoro.time)}</div>
+      <div>The timer is {timer ? "on" : "off"}</div>
       <div>
         Current timer interval:
         <input
           onChange={onChangeTimeInterval}
-          type='number'
+          type="number"
           value={timerInterval}
           min={4}
         />
       </div>
-      <button onClick={triggerTimer}>{timer ? 'OFF' : 'ON'}</button>
+      <button onClick={triggerTimer}>{timer ? "OFF" : "ON"}</button>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
