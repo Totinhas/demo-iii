220229117e18216087f7e52d53b85dac11c3a132
Sara Bretes <sarabretes@me.com>
2020-06-11 17:38:25 +0100

    Updates

---
+++ precache-manifest.87f461cffa129ae47ef89a81a1b8aaa6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a88cfc985b9093f18d44be3277cb390d",
+    "url": "/demo-iii/index.html"
+  },
+  {
+    "revision": "aedfa9ae6f16f2058fda",
+    "url": "/demo-iii/static/css/main.0a238190.chunk.css"
+  },
+  {
+    "revision": "d518094c89ee579706dd",
+    "url": "/demo-iii/static/js/2.3c39405b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/demo-iii/static/js/2.3c39405b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aedfa9ae6f16f2058fda",
+    "url": "/demo-iii/static/js/main.a37a73e0.chunk.js"
+  },
+  {
+    "revision": "1bf53e82559dfb9c7d6f",
+    "url": "/demo-iii/static/js/runtime-main.ea666a38.js"
+  }
+]);
